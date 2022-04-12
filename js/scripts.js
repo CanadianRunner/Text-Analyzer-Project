6b2f207b58389c2c848d19c2a190832dce064317
Sean Keane <seankeane93@gmail.com>
2022-04-11 19:31:32 -0700

    Update fourth test in JS

--- js/scripts.js
+++ js/scripts.js
@@ -2,14 +2,15 @@
  //Business Logic
 
  function wordCounter(text) {
-   if (text.length === 0) {
+   if (text.trim().length === 0) {
      return 0;
    }
    let wordCount = 0;
    const wordArray = text.split(" ");
-   wordArray.forEach(function(word) {
+   wordArray.forEach(function(element) {
+       if (!Number(element)) {
      wordCount++;
-   
+    }
    });
    return wordCount;
  }
