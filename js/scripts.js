737e4688bdd9614bdd158283b0bd1e741ce28fb5
Sean Keane <seankeane93@gmail.com>
2022-04-11 19:16:32 -0700

    add new test to check output equals 0

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,18 @@
 
-// Buisness Logic
-function wordCounter(text) {
-  let wordCount = 0;
-  const wordArray = text.split(" ");
-  wordArray.forEach(function(word) {
-    wordCount++;
-  });
-  return wordCount;
-}
+ //Business Logic
+
+ function wordCounter(text) {
+   if (text.length === 0) {
+     return 0;
+   }
+   let wordCount = 0;
+   const wordArray = text.split(" ");
+   wordArray.forEach(function(word) {
+     wordCount++;
+   
+   });
+   return wordCount;
+ }
+ 
+
+
